f60a1ef49a60de56ae5d350bcabed9483ac6884f
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-04-21 18:23:56 -0400

    fixing the migrations

--- db/migrate/20230414032056_drop_recipes_reviews_users_tables.rb
+++
@@ -1,7 +0,0 @@
-class DropRecipesReviewsUsersTables < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :recipes
-    drop_table :reviews
-    drop_table :users
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -30,10 +30,10 @@ ActiveRecord::Schema.define(version: 2023_04_14_153632) do
     t.string "filename", null: false
     t.string "content_type"
     t.text "metadata"
+    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", null: false
-    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
