a20ba122f3584492ac4f94998c9df51e94ab062b
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-04-23 13:32:56 -0400

    adding featured posts

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -5,24 +5,54 @@ import { UserContext } from "./UserContextProvider";
 import Login from "./Login";
 
 function HomePage() {
-  const { showLoginForm, setShowLoginForm } = useContext(UserContext);
-
+  const { showLoginForm, setShowLoginForm, user, posts } = useContext(UserContext);
+  console.log(posts)
+  let featuredPosts = [];
+  
+  if (posts && posts.length > 0) {
+    featuredPosts = posts.slice().sort((a, b) => b.comments.length - a.comments.length).slice(0, 4);
+  } 
+  
+ 
   return (
     <Wrapper>
       {showLoginForm && <Login setShowLoginModal={setShowLoginForm} />}
-      <ContactInformation>
-        <h5>
-          <a href="https://github.com/mendelr20">Mendel Rosenblum</a>
-          <br />
-          <br />
-          <a href="tel:973-650-4936">Number: 973-650-4936</a>
-          <br />
-          <br />
-          <a href="mailto:rosenblummm@gmail.com">
-            Email: Rosenblummm@gmail.com
-          </a>
-        </h5>
-      </ContactInformation>
+      {!user && <WelcomeMessage>Welcome to my blog!</WelcomeMessage>}
+      {user && posts && posts.length > 0 && featuredPosts.length > 0 && (
+        <FeaturedPostsContainer>
+          <h3>Featured Posts</h3>
+          <FeaturedPostsList>
+            {featuredPosts
+              .map((post) => (
+                <FeaturedPostItem key={post.id}>
+                  <h4>{post.title}</h4>
+                  {/* <p>{post.comments} comments</p> */}
+                </FeaturedPostItem>
+              ))}
+          </FeaturedPostsList>
+        </FeaturedPostsContainer>
+      )}
+      <ContactContainer>
+        <ContactInformation>
+          <h5>Contact Information</h5>
+          <ul>
+            <li>
+              <a href="https://github.com/mendelr20">Mendel Rosenblum</a>
+            </li>
+            <li>
+              <a href="tel:973-650-4936">Number: 973-650-4936</a>
+            </li>
+            <li>
+              <a href="mailto:rosenblummm@gmail.com">
+                Email: Rosenblummm@gmail.com
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/mendel-rosenblum/">LinkedIn</a>
+            </li>
+          </ul>
+        </ContactInformation>
+      </ContactContainer>
     </Wrapper>
   );
 }
@@ -36,27 +66,82 @@ const Wrapper = styled.section`
   justify-content: center;
 `;
 
-const ContactInformation = styled.div`
+const FeaturedPostsContainer = styled.div`
   margin-top: 48px;
+  padding: 24px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+`;
+
+const FeaturedPostsList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const FeaturedPostItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 16px;
+
+  h4 {
+    font-size: 24px;
+    margin: 0;
+  }
+
+  p {
+    font-size: 18px;
+    color: #666;
+    margin: 0;
+  }
+`;
+
+const ContactContainer = styled.div`
+  margin-top: 48px;
+  padding: 24px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+`;
+
+
+const WelcomeMessage = styled.h1`
+  font-size: 36px;
+  font-weight: bold;
+  margin-bottom: 48px;
+`;
+
+
+const ContactInformation = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
 
   h5 {
-    font-size: 18px;
-    text-align: center;
-    line-height: 1.6;
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 16px;
+  }
 
-    a {
-      color: #663399;
-      text-decoration: none;
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+
+    li {
+      font-size: 18px;
+      text-align: center;
+      line-height: 1.6;
       margin-bottom: 8px;
 
-      &:hover {
-        text-decoration: underline;
+      a {
+        color: #663399;
+        text-decoration: none;
+
+        &:hover {
+          text-decoration: underline;
+        }
       }
     }
   }
-`;
+};`
 
 export default HomePage;
